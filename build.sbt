78b63ed686909131157994b981500aac80ef885a
Alexey <lanceon-fake-email@example.fake>
2014-11-26 23:07:55 +0200

    sketches

--- build.sbt
+++ build.sbt
@@ -10,6 +10,9 @@ scalacOptions ++= Seq("-deprecation", "-unchecked")
 
 libraryDependencies ++= {
   Seq(
-    "ch.qos.logback" % "logback-classic" % "1.1.2"
+    "org.scala-lang.modules" %% "scala-xml" % "1.0.2",
+    "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.2",
+    "ch.qos.logback" % "logback-classic" % "1.1.2",
+    "org.scalaz" % "scalaz-core_2.10" % "7.1.0"
   )
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,100 @@
-import java.util.Date
+import reactive.{Observing, Timer, EventSource, EventStream}
+
+import scalaz._
+import Scalaz._
+
+trait Move
+case object MoveUp extends Move
+case object MoveDown extends Move
+case object MoveLeft extends Move
+case object MoveRight extends Move
+
+case class Pos(x: Int, y: Int)
+
+trait Field {
+  def width: Int
+  def height: Int
+  def contains(x: Int, y: Int): Boolean
+  def hasWall(x: Int, y: Int): Boolean
+}
+
+trait FieldWithRobot extends Field {
+  def robotPosition: Pos
+}
+
+class Robot {
+  var path: List[Pos] = Nil
+}
+
+object Field {
+
+  def draw(field: FieldWithRobot): String = {
+    (for {
+      y <- Range(0, field.height)
+      x <- Range(0, field.width)
+    } yield {
+      val pos = Pos(x,y)
+      (field.contains(x, y) ? { field.hasWall(x,y) ? "#" | ((field.robotPosition==pos) ? "+" | "o") } | " ") +
+        ((x === field.width-1) ? "\n" | "")
+    }).mkString
+  }
+
+}
+
+case object Tick
+
+class Simulator(field: FieldWithRobot) extends Loggable {
+
+  var score = 0
+
+  implicit val observing = new Observing {}
+  private val timer = new Timer
+  val ticks = timer.map(_ => Tick)
+
+  def start(): Unit = {
+
+    ticks.foreach{ _ =>
+      println("Tick")
+    }
+
+  }
+
+  def stop() = {
+
+  }
+
+
+}
+
+class SimpleField extends FieldWithRobot {
+
+  override def width = 10
+  override def height = 10
+  override def robotPosition = Pos(1,1)
+
+  override def contains(x: Int, y: Int) = {
+    x >=0 && x <= 9 && y >=0 && y <= 9
+  }
+
+  override def hasWall(x: Int, y: Int) = {
+    y == 4 && x >= 3 && x <= 7
+  }
+
+}
+
 
 
 object Main extends Loggable {
 
   def main(args: Array[String]) {
 
-    logger.info("First log message - info level")
-    logger.debug("First log message - debug level")
-    logger.trace("First log message - trace level")
+    val field = new SimpleField
+    println(Field.draw(field))
 
-    println("Started at " + new Date)
+    val sim = new Simulator(field)
+    sim.start()
+
+    Thread.sleep(6000)
 
   }
 
